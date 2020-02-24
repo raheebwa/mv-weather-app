d13e36c40afb0cfe13a0890824e2069cb99bd817
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-24 20:27:06 +0300

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
+
