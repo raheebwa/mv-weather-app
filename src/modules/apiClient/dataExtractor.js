9600e99f30379118f43b75a1ff2d88478bf1e039
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-26 04:29:54 +0300

    feat: Can process api data, resolve #3

--- src/modules/apiClient/dataExtractor.js
+++ src/modules/apiClient/dataExtractor.js
@@ -1,5 +1,21 @@
-const dataExtractor = {
+import apiConsumer from './apiConsumer';
 
+const dataExtractor = {
+  cleanedCityData() {
+    const myCity = JSON.parse(apiConsumer.returnCityData());
+
+    const result = {
+      name: myCity.name,
+      condition: myCity.weather[0].main,
+      desc: myCity.weather[0].description,
+      temp: myCity.main.temp,
+      pressure: myCity.main.pressure,
+      humidity: myCity.main.humidity,
+      visibility: myCity.visibility,
+    };
+
+    return result;
+  },
 };
 
 export default dataExtractor;
