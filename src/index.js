cc3d3eb160d6b3b0d3c8c1c8f05e414887772bcd
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-26 07:33:30 +0300

    feat: Can use search form. Defaults to Kampala

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import domMaster from './modules/domMaster';
+import utilities from './modules/utilities';
 
 domMaster.renderForecast();
-domMaster.toggleMetric();
+
+const searchForm = utilities.getEl('search-form');
+
+searchForm.addEventListener('submit', () => {
+  utilities.getEl('weather-data').innerHTML = '';
+  const city = utilities.getEl('search-city');
+  domMaster.renderForecast(city.value);
+});
