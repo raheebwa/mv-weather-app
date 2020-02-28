489144e3306ea5247848d0cf4ec584c6be726c7b
Aheebwa Ramadhan <raaheebwa@gmail.com>
2020-02-28 19:28:44 +0300

    feat: add units to fetch url

--- src/modules/apiClient/apiConsumer.js
+++ src/modules/apiClient/apiConsumer.js
@@ -1,7 +1,7 @@
 
 const apiConsumer = {
   async returnCityData(city) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4e08624b108ae2264bc0075454e1f595`, {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=4e08624b108ae2264bc0075454e1f595`, {
       mode: 'cors',
     });
     const weatherData = await response.json();
